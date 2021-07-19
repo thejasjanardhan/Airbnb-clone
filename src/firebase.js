a09442077a6532acd6ef8225d73c12ca8c267aa2
thejasjanardhan <thejasj435@gmail.com>
2021-07-19 19:24:51 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAj1wJnsWYTeBaaRflbKuFAeHO9PTX5-KY",
+  authDomain: "airbnb-clone-7043d.firebaseapp.com",
+  projectId: "airbnb-clone-7043d",
+  storageBucket: "airbnb-clone-7043d.appspot.com",
+  messagingSenderId: "778361059214",
+  appId: "1:778361059214:web:84860afb88141f3ba3d6df",
+};
